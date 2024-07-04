b64409b26ac7c85872bf27553c9d857a40de67a5
Kinh Nguyen <nguyenkinh@ytecongcong.com>
2024-07-05 00:40:59 +0200

    tbl AB

--- R/dplyr.R
+++ R/dplyr.R
@@ -1,8 +1,29 @@
-#' Plot a 2D matrix as lines
+#' Tabulate 2 variables with count and percentage
 #'
-#' @param a matrix which columns are to plotted
+#' @param .data data
+#' @param row row variable
+#' @param col column variable
+#' @param digits round the percentages
 #'
-#' @return
+#' @export
+tbl_AB <- function(.data, row, col, digits = 2) {
+  row <- rlang::enquo(row)
+  col <- rlang::enquo(col)
+  .data %>%
+    count(!!row, !!col) %>%
+    pivot_wider(names_from = !!col, values_from = n) %>%
+    mutate(across(-!!row, \(x) replace_na(x, replace = 0))) %>%
+    mutate(
+      n = rowSums(across(-!!row)),
+      across(-c(!!row, n), \(x) round(x / n, 2), .names = "row_prop_{.col}")
+    ) %>%
+    relocate(n, .after = last_col()) %>%
+    arrange(desc(n))
+}
+
+#' Matrix with names to long table with rowname as column
+#'
+#' @param m matrix with rowname
 #' @export
 long_matrix <- function(m)
 {
@@ -10,6 +31,6 @@ long_matrix <- function(m)
     stop("2D only pls")
   m %>% 
     as.data.frame() %>%
-    rownames_to_column() %>% 
-    pivot_longer(-rowname)
+    tibble::rownames_to_column() %>%
+      tidyr::pivot_longer(-rowname)
 }
